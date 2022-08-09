456d987faa9d6feeead09e421e6dffeb91533db7
Rana0 <itdocx1@gmail.com>
2022-08-09 06:10:18 -0700

    complete Female Models Wallpaper app Bugs Fix

--- app/src/main/java/com/brainsMedia/FMW/Fragments/IndianCategory.java
+++ app/src/main/java/com/brainsMedia/FMW/Fragments/IndianCategory.java
@@ -1,8 +1,11 @@
 package com.brainsMedia.FMW.Fragments;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,10 +13,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.brainsMedia.FMW.Adapters.WallpaperAdapter;
+import com.brainsMedia.FMW.MainActivity;
 import com.brainsMedia.FMW.R;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -25,12 +32,13 @@ import java.util.ArrayList;
 
 
 public class IndianCategory extends Fragment {
-
+    private Boolean onBackPressed = false;
     private ProgressBar indianProg;
     private DatabaseReference indianRef;
     private RecyclerView indianRV;
     private ArrayList<String> indianLIst;
     private WallpaperAdapter indianAdapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -40,6 +48,8 @@ public class IndianCategory extends Fragment {
 
         indianProg = view.findViewById(R.id.indianProgress);
         indianRV = view.findViewById(R.id.indianRecycler);
+
+
 
         getIndianData();
 
@@ -79,4 +89,36 @@ public class IndianCategory extends Fragment {
             }
         });
     }
+
+ /*   // Initializing Dialogue
+    private void getDialog() {
+
+
+        IndianDialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT );
+        IndianDialog.setCancelable(false);
+
+        ImageButton share_btn = IndianDialog.findViewById(R.id.share_btn);
+        Button close_btn = IndianDialog.findViewById(R.id.close_btn);
+        TextView contact_txt = IndianDialog.findViewById(R.id.email);
+
+        share_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shareApp();
+
+            }
+        });
+
+        close_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IndianDialog.dismiss();
+            }
+        });
+
+    }
+
+  */
+
+
 }
